2c44686a2f055ac57d2e304680f2c50b061bb13d
Madhur Pathak <madhurpathak000@gmail.com>
2023-12-03 23:17:45 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Search, ShoppingCart } from '@mui/icons-material';
+import { Badge } from '@mui/material';
+
 import React from 'react'
 import styled from 'styled-components'
 
@@ -10,36 +13,74 @@ const Wrapper = styled.div`
 padding: 10px 20px;
 display:flex;
 justify-content:space-between;
+align-items: center;
 `;
 
-const Left = styled.div `
+const Left = styled.div`
 flex:1;
+display: flex;
+align-items: center;
 `;
 
 const Language = styled.span`
 font-size: 14px;
 cursor: pointer;
 `
+const SearchContainer = styled.div`
+   border :0.5px solid lightgray  ;
+   display: flex;
+   align-items: center;
+   margin-left: 25px;
+   padding: 5px ;
+`
+const Input = styled.input`
+  border: none;
+`
 
-
-const Center= styled.div `
+const Center = styled.div`
 flex:1;
 `;
-const Right = styled.div `
+
+const Logo = styled.h1`
+  font-weight: bold;
+  text-align: center;
+`
+const Right = styled.div`
 flex:1;
+display: flex;
+align-items: center;
+justify-content: flex-end;
 `;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`
 
 
 const Navbar = () => {
   return (
     <Container>
-     <Wrapper>
+      <Wrapper>
         <Left>
-            <Language>EN</Language>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <Search style={{color:'gray',fontSize:'16px'}}></Search>
+          </SearchContainer>
         </Left>
-        <Center>Center</Center>
-        <Right>Right</Right>
-        </Wrapper>   
+        <Center><Logo>E-Com_App</Logo></Center>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Login</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCart></ShoppingCart>
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
     </Container>
   )
 }
